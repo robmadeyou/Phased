9e8b3b0109e78e3572c1433cf7e6fa43f9fabfd4
robmadeyou <robmadeyou@gmail.com>
2016-01-07 17:12:03 +0000

    Removed action added Command

---
+++ Server/src/server/commands/Command.java
@@ -0,0 +1,20 @@
+package server.commands;
+
+/**
+ * Created by r on 07/01/16.
+ */
+public class Command
+{
+
+    private String activation;
+
+    public Command ()
+    {
+        super ();
+    }
+
+    public String getActivation()
+    {
+        return this.activation;
+    }
+}
