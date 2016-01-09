233552526c8eeab7176027816544e8d3106c25d6
robmadeyou <robmadeyou@gmail.com>
2016-01-09 02:07:07 +0000

    Intellij didn't like player command :(

---
+++ Server/src/server/commands/PlayerCommand.java
@@ -0,0 +1,25 @@
+package server.commands;
+
+import server.model.players.Client;
+
+/**
+ * Created by r on 09/01/2016.
+ */
+public class PlayerCommand extends Command {
+
+    public Client player;
+    public PlayerCommand(String activation, CommandExecute onActivate) {
+        super(activation, onActivate);
+    }
+
+    public void setPlayer( Client c )
+    {
+        this.player = c;
+    }
+
+    public final Client getPlayer()
+    {
+        return this.player;
+    }
+
+}
